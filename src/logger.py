3433c0442b127ee32a947e94e1a17419b3331c4d
Your Name <you@example.com>
2024-02-07 14:55:10 +0100

    Monitoring servers in progress

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,10 @@
+import os
+import time
 from datetime import datetime
 from multiprocessing import Lock
 
 _lock = Lock()
+_upd_dct = {}
 
 def get_now_tm_str():
     now = datetime.now()
@@ -9,13 +12,34 @@ def get_now_tm_str():
                                                    now.hour, now.minute, now.second, now.microsecond)
 
 def create_or_clear_file(filename):
+    if os.path.exists(filename):
+        os.system(f'mv {filename} old_{filename}')
     f = open(filename, 'w')
 
-def log_in_file(filename, text):
+def log_in_file(filename, text, typ='INFO'):
     global _lock
+    global _upd_dct
+    #
+    if filename[-4:] != '.csv':
+        filename += '.csv'
+    #
     _lock.acquire()
     now_tm_str = get_now_tm_str()
     f = open(filename, 'a')
-    f.write('%s;%s\n' % (now_tm_str, text))
+    f.write('%s;%s;%s\n' % (now_tm_str, typ, text))
     f.close()
+    #
+    if _upd_dct.get(filename) is None:
+        _upd_dct[filename] = time.time()
+    #if time.time() - _upd_dct[filename] > 12 * 3600:
+    tm_secs = int(time.time())
+    #if tm_secs % (12 * 3600) == 0:
+    #    os.system(f'mv {filename} old_{filename}')
+    #    f = open(filename, 'w')
+    #    f.write('%s;%s\n' % (now_tm_str, ''))
+    #    f.close()
+    #    #_upd_dct[filename] = time.time()
+    #
     _lock.release()
+
+
